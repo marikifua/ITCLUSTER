9ccf927f822d1d5a4b3654a71f379585dc4ffe5a
marikifua <marik_if_ua@ukr>
2019-03-20 11:39:35 +0200

    Change network in private and public subnets

--- variables.tf
+++ variables.tf
@@ -21,13 +21,13 @@ variable "itcluster_vpc_cidr" {
 variable "public_subnet_cidr" {
     type = "list"
     description = "Subnet CIDRs for public subnets"
-    default = ["10.0.1.0/24", "10.0.2.0/24"]
+    default = ["10.0.1.0/24"]
 }
 
 variable "private_subnet_cidr" {
     type = "list"
     description = "Subnet CIDRs for private subnets"
-    default = ["10.0.10.0/24", "10.0.20.0/24"]
+    default = ["10.0.10.0/24"]
 }
 variable "db_replication" {
     type = "list"
